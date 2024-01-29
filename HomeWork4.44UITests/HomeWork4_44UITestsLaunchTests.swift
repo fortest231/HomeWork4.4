6d8adcfc4b65840d5c13d9e75efe66c1dae1636d
fortest231 <146324430+fortest231@users.noreply.github.com>
2024-01-29 12:29:49 +0600

    Initial Commit

---
+++ HomeWork4.44UITests/HomeWork4_44UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeWork4_44UITestsLaunchTests.swift
+//  HomeWork4.44UITests
+//
+//  Created by MacBook Pro on 29/1/24.
+//
+
+import XCTest
+
+final class HomeWork4_44UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
