772b9c8b6393b58229e5d64066fb81a3b5a0855b
fortest231 <146324430+fortest231@users.noreply.github.com>
2024-01-29 15:24:03 +0600

    homeWork4

---
+++ HomeWork4.44/Common/Struct.swift
@@ -0,0 +1,15 @@
+//
+//  Struct.swift
+//  HomeWork4.44
+//
+//  Created by MacBook Pro on 29/1/24.
+//
+
+import Foundation
+
+
+struct Struct {
+    var image: String
+    var text: String
+    var liltext: String
+}
